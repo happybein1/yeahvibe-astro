51fb14baa8e18bdafc291499b43cabe5625ace18
happybein1 <happybein1@gmail.com>
2026-05-12 21:09:26 +0200

    new start here page

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const SITE_VERSION = '1.1.0';
+export const SITE_VERSION = '1.2.0';
 export const SITE_VERSION_DATE = '2026-05-13';
-export const SITE_VERSION_NOTES = 'Added Vimeo videos to Foresight Trends and Fair Globe Sharing; renamed Applications to YeahSight';
+export const SITE_VERSION_NOTES = 'Rewrote Start Here with 5 Pillars of Leverage; added Vimeo videos; renamed Applications to YeahSight';
