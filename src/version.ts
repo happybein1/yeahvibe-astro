e6d799d124dd73d7b046490db5f40c597b327619
happybein1 <happybein1@gmail.com>
2026-05-14 11:29:42 +0200

    OG

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const SITE_VERSION = '1.3.0';
-export const SITE_VERSION_DATE = '2026-05-13';
-export const SITE_VERSION_NOTES = 'Start Here rewritten with 5 Pillars; remedy/insight always visible; sidebar subsections expanded by default';
+export const SITE_VERSION = '1.3.1';
+export const SITE_VERSION_DATE = '2026-05-14';
+export const SITE_VERSION_NOTES = 'Added OG image for social sharing';
