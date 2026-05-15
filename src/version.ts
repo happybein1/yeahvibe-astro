9eb63ce9a8fcf0e3249ae8ed85c8a1229eb61ed6
happybein1 <happybein1@gmail.com>
2026-05-15 18:36:27 +0200

    links update

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const SITE_VERSION = '1.6.1';
+export const SITE_VERSION = '1.6.2';
 export const SITE_VERSION_DATE = '2026-05-15';
-export const SITE_VERSION_NOTES = 'Created Welcome page from homepage content; index now redirects to welcome; sidebar reordered with 5 Pillars after Advised Tools';
+export const SITE_VERSION_NOTES = 'Mobile header: GitHub/Discord moved to footer; language select made compact; Welcome page created';
