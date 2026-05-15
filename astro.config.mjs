0f6c2c36e6a61e32b63fd5c339091c662d3fa653
happybein1 <happybein1@gmail.com>
2026-05-15 18:14:55 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,7 +44,7 @@ export default defineConfig({
                     collapsed: false,
                     items: [
                         { label: 'Pillar 1: Direction', translations: { fr: 'Pilier 1 : Direction' }, slug: 'pillars/direction' },
-                        { label: 'Pillar 2: Mindset', translations: { fr: 'Pilier 2 : État d'esprit' }, slug: 'pillars/mindset' },
+                        { label: 'Pillar 2: Mindset', translations: { fr: "Pilier 2 : État d'esprit" }, slug: 'pillars/mindset' },
                         { label: 'Pillar 3: Organization', translations: { fr: 'Pilier 3 : Organisation' }, slug: 'pillars/organization' },
                         { label: 'Pillar 4: Collaboration', translations: { fr: 'Pilier 4 : Collaboration' }, slug: 'pillars/collaboration' },
                         { label: 'Pillar 5: YeahSight', translations: { fr: 'Pilier 5 : YeahSight' }, slug: 'pillars/yeahsight' },
