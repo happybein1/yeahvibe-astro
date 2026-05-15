7136794badc1e79a2d85ebe499b5a135e047e3cd
happybein1 <happybein1@gmail.com>
2026-05-15 23:28:31 +0200

    jbh

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const SITE_VERSION = '1.6.2';
+export const SITE_VERSION = '1.6.3';
 export const SITE_VERSION_DATE = '2026-05-15';
-export const SITE_VERSION_NOTES = 'Mobile header: GitHub/Discord moved to footer; language select made compact; Welcome page created';
+export const SITE_VERSION_NOTES = 'Mobile header: replaced garbled language selector with clean EN/FR text toggle; GitHub/Discord in footer';
