63160b671ada1d51013c308675c85eb99e10265e
happybein1 <happybein1@gmail.com>
2026-05-27 07:22:49 +0200

    try to fix

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const SITE_VERSION = '1.6.8';
 export const SITE_VERSION_DATE = '2026-05-26';
-export const SITE_VERSION_NOTES = 'Welcome page: video halved to 160px, text layout on right on desktop; stacks on mobile';
+export const SITE_VERSION_NOTES = 'Welcome page: 9:16 video (220px) left + text right layout via custom.css; mobile stacks vertically';
